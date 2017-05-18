ec7fe739284aa83fd5b893fb8d1a534ab56fc3ad
ebracho <eddieb@liatrio.com>
2017-05-17 17:47:10 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## Pipeline Monkey
+
+A tool for exercising CI pipelines.
+
+### Features:
+
+* Commit and push changes to git repos (coming soon)
+* Create and trigger Jenkins build jobs (coming soon)
+
+### Configuration
+
+Configuration goes in *config.json*
+
+* *repos_dir* - Path to directory containing git repositories. Commits will be added and pushed from these repos.
+* *jenkins_url* - Url to the jenkins instance being exercised. Jobs will be created and triggered here.
+* *jenkins_user* - Username to authenticate with Jenkins.
+* *jenkins_token* - Token to authenticate with Jenkins.
+
---
+++ monkey.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import datetime
+import json
+
+root_dir = os.path.dirname(os.path.realpath(__file__))
+
+with open(os.path.join(root_dir, 'config.json')) as f:
+    config = json.load(f)
+
+def get_repos():
+    res = subprocess.run(['ls', config['repos_dir']], stdout=subprocess.PIPE)
+    return res.stdout.decode('utf-8').split()
+
+def push_empty_commit(repo):
+    repo = os.path.join(config['repos_dir'], repo)
+    time = datetime.datetime.now().strftime('%c')
+    script = """
+    echo Empty Commit - {time} >>{repo}/monkey.log;
+    git -C {repo} add {repo}/monkey.log;
+    git -C {repo} commit -m "Pipeline Monkey - {time}";
+    git -C {repo} push;
+    """.format(repo=repo, time=time)
+    subprocess.run(['sh', '-c', script])
+
+push_empty_commit('test')
